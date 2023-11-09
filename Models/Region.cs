aae503a7f0f4b0878aa269e0420da6f8a1fe7d58
Thiago Arcoverde <tluiz2701@gmail.com>
2023-11-09 17:58:51 -0300

    Changed css style and added region filter with optimized api request in the initialization

---
+++ Models/Region.cs
@@ -0,0 +1,16 @@
+﻿namespace Pokedex.Models
+{
+    public class Region
+    {
+        public List<Pokemon> Kanto { get; set; } = new List<Pokemon>();
+        public List<Pokemon> Johto { get; set; } = new List<Pokemon>();
+        public List<Pokemon> Hoenn { get; set; } = new List<Pokemon>();
+        public List<Pokemon> Sinnoh { get; set; } = new List<Pokemon>();
+        public List<Pokemon> Unova { get; set; } = new List<Pokemon>();
+        public List<Pokemon> Kalos { get; set; } = new List<Pokemon>();
+        public List<Pokemon> Alola { get; set; } = new List<Pokemon>();
+        public List<Pokemon> Galar { get; set; } = new List<Pokemon>();
+        public List<Pokemon> Paldea { get; set; } = new List<Pokemon>();
+
+    }
+}
--- Util/PokeClient.cs
+++ Util/PokeClient.cs
@@ -11,11 +11,137 @@ namespace Pokedex.Util
             Client = client;
         }
 
-        public async Task<Pokemon> GetPokemon(string id)
+        public async Task<Pokemon?> GetPokemon(string id)
         {
             var response = await Client.GetAsync($"https://pokeapi.co/api/v2/pokemon/{id}");
             var content = await response.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<Pokemon>(content);
+        }
+
+        public async Task<List<Pokemon>> GetKantoPokemon()
+        {
+            var pokemonList = new List<Pokemon>();
+            for (int i = 1; i <= 151; i++)
+            {
+                Pokemon? pokemon = await GetPokemon(i.ToString());
+                if (pokemon != null)
+                {
+                    pokemonList.Add(pokemon);
+                }
+            }
+            return pokemonList;
+        }
+
+        public async Task<List<Pokemon>> GetJohtoPokemon()
+        {
+            var pokemonList = new List<Pokemon>();
+            for (int i = 152; i <= 251; i++)
+            {
+                Pokemon? pokemon = await GetPokemon(i.ToString());
+                if (pokemon != null)
+                {
+                    pokemonList.Add(pokemon);
+                }
+            }
+            return pokemonList;
+        }
+
+        public async Task<List<Pokemon>> GetHoennPokemon()
+        {
+            var pokemonList = new List<Pokemon>();
+            for (int i = 252; i <= 386; i++)
+            {
+                Pokemon? pokemon = await GetPokemon(i.ToString());
+                if (pokemon != null)
+                {
+                    pokemonList.Add(pokemon);
+                }
+            }
+            return pokemonList;
+        }
+
+        public async Task<List<Pokemon>> GetSinnohPokemon()
+        {
+            var pokemonList = new List<Pokemon>();
+            for (int i = 387; i <= 493; i++)
+            {
+                Pokemon? pokemon = await GetPokemon(i.ToString());
+                if (pokemon != null)
+                {
+                    pokemonList.Add(pokemon);
+                }
+            }
+            return pokemonList;
+        }
+
+        public async Task<List<Pokemon>> GetUnovaPokemon()
+        {
+            var pokemonList = new List<Pokemon>();
+            for (int i = 494; i <= 694; i++)
+            {
+                Pokemon? pokemon = await GetPokemon(i.ToString());
+                if (pokemon != null)
+                {
+                    pokemonList.Add(pokemon);
+                }
+            }
+            return pokemonList;
+        }
+
+        public async Task<List<Pokemon>> GetKalosPokemon()
+        {
+            var pokemonList = new List<Pokemon>();
+            for (int i = 650; i <= 721; i++)
+            {
+                Pokemon? pokemon = await GetPokemon(i.ToString());
+                if (pokemon != null)
+                {
+                    pokemonList.Add(pokemon);
+                }
+            }
+            return pokemonList;
+        }
+
+        public async Task<List<Pokemon>> GetAlolaPokemon()
+        {
+            var pokemonList = new List<Pokemon>();
+            for (int i = 722; i <= 809; i++)
+            {
+                Pokemon? pokemon = await GetPokemon(i.ToString());
+                if (pokemon != null)
+                {
+                    pokemonList.Add(pokemon);
+                }
+            }
+            return pokemonList;
+        }
+
+        public async Task<List<Pokemon>> GetGalarPokemon()
+        {
+            var pokemonList = new List<Pokemon>();
+            for (int i = 810; i <= 905; i++)
+            {
+                Pokemon? pokemon = await GetPokemon(i.ToString());
+                if (pokemon != null)
+                {
+                    pokemonList.Add(pokemon);
+                }
+            }
+            return pokemonList;
+        }
+
+        public async Task<List<Pokemon>> GetPaldeaPokemon()
+        {
+            var pokemonList = new List<Pokemon>();
+            for (int i = 906; i <= 1017; i++)
+            {
+                Pokemon? pokemon = await GetPokemon(i.ToString());
+                if (pokemon != null)
+                {
+                    pokemonList.Add(pokemon);
+                }
+            }
+            return pokemonList;
         }
     }
 }
